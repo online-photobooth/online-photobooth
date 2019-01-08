0a2cc9df7114ede0574f51ed4be9ff2fb7fe2ca7
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2019-01-08 18:53:09 +0100

    Loader

--- src/components/buttons/CaptureButton.js
+++ src/components/buttons/CaptureButton.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class CaptureButton extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            isClicked: false,
+        };
+    }
+    
     onClick = () => {
+        this.setState({ isClicked: true })
         this.props.onClick()
     }
     
     render = () => {
         return (
-            <button className='CaptureButton' onClick={ this.onClick }>
+            <button className={ `CaptureButton ${ this.state.isClicked ? 'clicked' : '' }` } onClick={ this.onClick }>
                 <i className="fas fa-camera-retro"></i>
             </button>
         )
