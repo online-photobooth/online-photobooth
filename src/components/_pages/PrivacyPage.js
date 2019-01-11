7b9c327d445ea90d566a729106239f38b204db52
Jordy Pereira <jordy.pereira@hotmail.com>
2019-01-11 13:48:59 +0100

    privacy

--- src/components/_pages/PrivacyPage.js
+++ src/components/_pages/PrivacyPage.js
@@ -3,11 +3,13 @@ import React from 'react';
 class PrivacyPage extends React.Component {
     render = () => {
         return (
-            <div>
-              <h1>Privacy Policy Google Photos Photobooth</h1>
-              <p>Welcome to the Google Photos Photobooth. If you login with your account, we will load in your shared albums. You can select or create a new album.</p>
-              <p>You can take a picture and upload it to the selected album. Then you can send a mail with the picture attached from you gmail which you've logged in with.</p>
-              <p>We do not save any of this data and it's only used in your current session.</p>
+            <div className="wrapper">
+              <div>
+                <h1>Privacy Policy Google Photos Photobooth</h1>
+                <p>Welcome to the Google Photos Photobooth. If you login with your account, we will load in your shared albums. You can select or create a new album.</p> <br></br>
+                <p>You can take a picture and upload it to the selected album. Then you can send a mail with the picture attached and a link to the shared album, from you gmail which you've logged in with. There's also a QR code that links to the shared album.</p> <br></br>
+                <p>We do not save any of this data and it's only used in your current session.</p>
+              </div>
             </div>
         )
     }
