ef1195768dcd6cd11b4210b6c0738aeae13922bc
Jordy Pereira <jordy.pereira@prorenata.se>
2019-05-21 11:53:36 +0200

    Delete serviceworker

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import throttle from 'lodash/throttle';
-import { loadState, saveState } from './localStorage';
+// import throttle from 'lodash/throttle';
+// import { loadState, saveState } from './localStorage';
 
 const initialState = {
   accessToken: '',
@@ -12,7 +12,7 @@ const initialState = {
   expiresAt: '',
 };
 
-const persistedState = loadState();
+// const persistedState = loadState();
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -56,10 +56,11 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer, persistedState);
+// const store = createStore(reducer, persistedState);
+const store = createStore(reducer);
 
-store.subscribe(throttle(() => {
-  saveState(store.getState());
-}, 1000));
+// store.subscribe(throttle(() => {
+//   saveState(store.getState());
+// }, 1000));
 
 export default store;
