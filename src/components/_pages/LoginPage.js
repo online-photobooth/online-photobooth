347014d8dd43cf84eeb2cac042ab26b1e6f720e1
Jordy Pereira <jordy.pereira@prorenata.se>
2019-05-27 21:12:01 +0200

    Login page

--- src/components/_pages/LoginPage.js
+++ src/components/_pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { connect } from 'react-redux';
+import { css } from 'emotion';
 import BaseButton from '../buttons/BaseButton';
 import Heading from '../titles/Heading';
 
@@ -33,26 +34,68 @@ class LoginPage extends React.Component {
   }
 
   render = () => (
-    <div className="flex">
-      <img src="/style/img/movie-strip.png" alt="Movie Strip" />
-      <div className="">
-        <Heading type="heading--2">Welkom op de KdG Photobooth.</Heading>
+    <div className={
+      css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    `}
+    >
+      <div className={
+        css`
+        display: flex;
+        justify-content: center;
+        height: 60%;
+      `}
+      >
+        <div
+          className={
+            css`
+            margin-right: 40px;
+          `}
+        >
+          <img
+            className={
+              css`
+            max-height: 100%;
+          `}
+            src="/style/img/movie-strip.png"
+            alt="Movie Strip"
+          />
+        </div>
+        <div className={
+          css`
+        display: flex;
+        flex-direction: column;
+      `}>
+          <Heading type="heading--3">Welkom op de KdG photobooth.</Heading>
 
-        <GoogleLogin
-          clientId={process.env.REACT_APP_CLIENT_ID}
-          onSuccess={resp => this.successResponseGoogle(resp)}
-          onFailure={resp => this.responseGoogle(resp)}
-          scope="profile email https://www.googleapis.com/auth/photoslibrary https://www.googleapis.com/auth/photoslibrary.sharing https://mail.google.com/"
-          prompt="consent"
-          loginHint="photobooth@kdg.be"
-          render={renderProps => (
-            <BaseButton
-              onClick={renderProps.onClick}
-            >
-              Let's get this party started!
-            </BaseButton>
-          )}
-        />
+          <div className={
+            css`
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+           `}
+          >
+            <GoogleLogin
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              onSuccess={resp => this.successResponseGoogle(resp)}
+              onFailure={resp => this.responseGoogle(resp)}
+              scope="profile email https://www.googleapis.com/auth/photoslibrary https://www.googleapis.com/auth/photoslibrary.sharing https://mail.google.com/"
+              prompt="consent"
+              loginHint="photobooth@kdg.be"
+              render={renderProps => (
+                <BaseButton
+                  onClick={renderProps.onClick}
+                >
+                  Let's get this party started!
+                </BaseButton>
+              )}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
